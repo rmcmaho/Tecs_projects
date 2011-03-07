3adc2fc39b11a9ac355999f1c9c7aac7423d4246
Robbie McMahon <robbie@Mega-Oge.local>
2011-03-06 22:59:50 -0800

    Made the file for the Fill program.

---
+++ project 04/fill/Fill.asm
@@ -0,0 +1,6 @@
+// I/O handling: This program runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen, i.e. writes "black" in every pixel.
+// When no key is pressed, the screen should be cleared.
+
+// You may choose to blacken and clear the screen in any visual order, as long as pressing a key continuously for long enough will result in a fully blackened screen and not pressing any key for long enough will result in a cleared screen.
+// This program has a test script (Fill.tst) but no compare file – it should be checked visually by inspecting the simulated screen.

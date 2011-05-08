350041adc885b6927f0e49ac59ae4d8d49d5337f
Robbie McMahon <mcmahon.robbie@gmail.com>
2011-05-08 12:48:09 -0700

    Added some comments.
    Contained the output writing in a 'with' clause.
    This will automatically close the file upon completion.

--- project 06/assembler/main.py
+++ project 06/assembler/main.py
@@ -5,20 +5,27 @@ import HackAssemblyParser
 import HackBinaryCode
 
 def main():
+    # Get the input filename (required)
     inputFileName = sys.argv[1]
+    # Set the default output filename
     outputFileName = 'a.out'
+    # Try to get the output filename
     try:
         outputFileName = sys.argv[2]
     except IndexError:
         pass
-    outputFile = open (outputFileName, 'w')
+    # Parse the input file
     parser = HackAssemblyParser.parseAssembly(inputFileName)
-    for op in parser:
-        print op
-        binaryCommand = HackBinaryCode.convertCommand(op)
-        print binaryCommand
-        outputFile.write(binaryCommand)
-        outputFile.write('\n')
+    # Open the output file for writing
+    with open (outputFileName, 'w') as outputFile:
+        # Step through the parser and convert each command
+        for op in parser:
+            print op
+            binaryCommand = HackBinaryCode.convertCommand(op)
+            print binaryCommand
+            outputFile.write(binaryCommand)
+            outputFile.write('\n')
+    # The output file will be automatically closed upon leaving the 'with' clause
 
 
 if __name__ == '__main__':

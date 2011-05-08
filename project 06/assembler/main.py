4536c70b504b886ae82b8c0c144967a245e030dd
Robbie McMahon <mcmahon.robbie@gmail.com>
2011-05-08 12:43:58 -0700

    My assembler successfully generated the same output as the official assembler.
    Next step: symbol table

--- project 06/assembler/main.py
+++ project 06/assembler/main.py
@@ -5,11 +5,20 @@ import HackAssemblyParser
 import HackBinaryCode
 
 def main():
-    fileName = sys.argv[1]
-    parser = HackAssemblyParser.parseAssembly(fileName)
+    inputFileName = sys.argv[1]
+    outputFileName = 'a.out'
+    try:
+        outputFileName = sys.argv[2]
+    except IndexError:
+        pass
+    outputFile = open (outputFileName, 'w')
+    parser = HackAssemblyParser.parseAssembly(inputFileName)
     for op in parser:
         print op
-        print HackBinaryCode.convertCommand(op)
+        binaryCommand = HackBinaryCode.convertCommand(op)
+        print binaryCommand
+        outputFile.write(binaryCommand)
+        outputFile.write('\n')
 
 
 if __name__ == '__main__':

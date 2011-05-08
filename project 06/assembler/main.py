9434815fa044bd9b4bf03da1c5d0b5d366296001
Robbie McMahon <mcmahon.robbie@gmail.com>
2011-05-08 12:08:02 -0700

    Added the conversion to the main module to test it.

--- project 06/assembler/main.py
+++ project 06/assembler/main.py
@@ -2,13 +2,14 @@
 
 import sys
 import HackAssemblyParser
-
+import HackBinaryCode
 
 def main():
     fileName = sys.argv[1]
     parser = HackAssemblyParser.parseAssembly(fileName)
     for op in parser:
         print op
+        print HackBinaryCode.convertCommand(op)
 
 
 if __name__ == '__main__':

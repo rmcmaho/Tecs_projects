572a89cc14ae6b064a40d40f4f405d863d8ae0ee
Robbie McMahon <mcmahon.robbie@gmail.com>
2011-05-10 22:08:24 -0700

    Made a function to generate a symbol table. It is untested.

--- project 06/assembler/HackSymbolTable.py
+++ project 06/assembler/HackSymbolTable.py
@@ -1,6 +1,6 @@
 
 class SymbolTable:
-
+    ''' Class to represent a symbol table'''
     symbolTable = {"SP"     : 0,
                    "LCL"    : 1,
                    "ARG"    : 2,
@@ -43,3 +43,47 @@ class SymbolTable:
 
     def getAddress (self, symbol):
         return self.symbolTable[symbol]
+# END SymbolTable
+
+
+def generateSymbolTable (inputFileName):
+    ''' Function to generate a symbol table from an assembly file '''
+    assemblyFile = file(inputFileName)
+    lineNumber = 0
+    symbolTable = SymbolTable()
+    for line in assemblyFile:
+        # Strip off leading and trailing whitespace
+        line = line.strip()
+
+        if len(line) == 0:
+            # Ignore blank lines
+            continue
+        elif line.startswith("@"):
+            # Increment line number for A commands
+            lineNumber += 1
+            # Symbol starts after @
+            aSymbol = line[1:]
+            # If the symbol doesn't already exist
+            if (not symbolTable.contains(aSymbol)):
+                # Then add it
+                symbolTable.addEntry(aSymbol)
+            continue
+        elif line.startswith("("):
+            # Do not increment line number for labels
+            pass
+            # Label begins after first ( and ends before )
+            label = line[1:-1]
+            # Remove leading or trailing whitespace from the label
+            label = label.strip()
+            # If the label doesn't already exist
+            if (not symbolTable.contains(label)):
+                # Then add it as the line number
+                symbolTable.addEntry(label, lineNumber)
+            continue
+        elif line.startswith("//"):
+            # Ignore comments
+            continue
+        else:
+            # Incrememnt line number for C commands
+            lineNumber += 1
+
